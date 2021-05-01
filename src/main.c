50dcfca81e94734ce1600e475422c8065322b522
Gustav Louw <airglouw@gmail.com>
2021-04-30 20:53:06 -0700

    ONE LESS FILE

--- src/main.c
+++ src/main.c
@@ -156,7 +156,124 @@ PromoteLTE(Elem a, Elem b)
 static void
 Cast(Elem e, Type type)
 {
-#include "cast.c"
+    e->type = I8; 
+    switch(type)
+    {
+    case I8: 
+        switch(e->type)
+        {
+        case  I8: e->poly.i8 = e->poly.i8;  break; case  U8: e->poly.i8 = e->poly.u8;  break;
+        case I16: e->poly.i8 = e->poly.i16; break; case U16: e->poly.i8 = e->poly.u16; break;
+        case I32: e->poly.i8 = e->poly.i32; break; case U32: e->poly.i8 = e->poly.u32; break;
+        case I64: e->poly.i8 = e->poly.i64; break; case U64: e->poly.i8 = e->poly.u64; break;
+        case F32: e->poly.i8 = e->poly.f32; break; case F64: e->poly.i8 = e->poly.f64; break;
+        default: goto error;
+        }
+        break;
+    case U8: 
+        switch(e->type)
+        {
+        case  I8: e->poly.u8 = e->poly.i8;  break; case  U8: e->poly.u8 = e->poly.u8;  break;
+        case I16: e->poly.u8 = e->poly.i16; break; case U16: e->poly.u8 = e->poly.u16; break;
+        case I32: e->poly.u8 = e->poly.i32; break; case U32: e->poly.u8 = e->poly.u32; break;
+        case I64: e->poly.u8 = e->poly.i64; break; case U64: e->poly.u8 = e->poly.u64; break;
+        case F32: e->poly.u8 = e->poly.f32; break; case F64: e->poly.u8 = e->poly.f64; break;
+        default: goto error;
+        }
+        break;
+    case I16:
+        switch(e->type)
+        {
+        case  I8: e->poly.i16 = e->poly.i8;  break; case  U8: e->poly.i16 = e->poly.u8;  break;
+        case I16: e->poly.i16 = e->poly.i16; break; case U16: e->poly.i16 = e->poly.u16; break;
+        case I32: e->poly.i16 = e->poly.i32; break; case U32: e->poly.i16 = e->poly.u32; break;
+        case I64: e->poly.i16 = e->poly.i64; break; case U64: e->poly.i16 = e->poly.u64; break;
+        case F32: e->poly.i16 = e->poly.f32; break; case F64: e->poly.i16 = e->poly.f64; break;
+        default: goto error;
+        }
+        break;
+    case U16:
+        switch(e->type)
+        {
+        case  I8: e->poly.u16 = e->poly.i8;  break; case  U8: e->poly.u16 = e->poly.u8;  break;
+        case I16: e->poly.u16 = e->poly.i16; break; case U16: e->poly.u16 = e->poly.u16; break;
+        case I32: e->poly.u16 = e->poly.i32; break; case U32: e->poly.u16 = e->poly.u32; break;
+        case I64: e->poly.u16 = e->poly.i64; break; case U64: e->poly.u16 = e->poly.u64; break;
+        case F32: e->poly.u16 = e->poly.f32; break; case F64: e->poly.u16 = e->poly.f64; break;
+        default: goto error;
+        }
+        break;
+    case I32:
+        switch(e->type)
+        {
+        case  I8: e->poly.i32 = e->poly.i8;  break; case  U8: e->poly.i32 = e->poly.u8;  break;
+        case I16: e->poly.i32 = e->poly.i16; break; case U16: e->poly.i32 = e->poly.u16; break;
+        case I32: e->poly.i32 = e->poly.i32; break; case U32: e->poly.i32 = e->poly.u32; break;
+        case I64: e->poly.i32 = e->poly.i64; break; case U64: e->poly.i32 = e->poly.u64; break;
+        case F32: e->poly.i32 = e->poly.f32; break; case F64: e->poly.i32 = e->poly.f64; break;
+        default: goto error;
+        }
+        break;
+    case U32:
+        switch(e->type)
+        {
+        case  I8: e->poly.u32 = e->poly.i8;  break; case  U8: e->poly.u32 = e->poly.u8;  break;
+        case I16: e->poly.u32 = e->poly.i16; break; case U16: e->poly.u32 = e->poly.u16; break;
+        case I32: e->poly.u32 = e->poly.i32; break; case U32: e->poly.u32 = e->poly.u32; break;
+        case I64: e->poly.u32 = e->poly.i64; break; case U64: e->poly.u32 = e->poly.u64; break;
+        case F32: e->poly.u32 = e->poly.f32; break; case F64: e->poly.u32 = e->poly.f64; break;
+        default: goto error;
+        }
+        break;
+    case I64:
+        switch(e->type)
+        {
+        case  I8: e->poly.i64 = e->poly.i8;  break; case  U8: e->poly.i64 = e->poly.u8;  break;
+        case I16: e->poly.i64 = e->poly.i16; break; case U16: e->poly.i64 = e->poly.u16; break;
+        case I32: e->poly.i64 = e->poly.i32; break; case U32: e->poly.i64 = e->poly.u32; break;
+        case I64: e->poly.i64 = e->poly.i64; break; case U64: e->poly.i64 = e->poly.u64; break;
+        case F32: e->poly.i64 = e->poly.f32; break; case F64: e->poly.i64 = e->poly.f64; break;
+        default: goto error;
+        }
+        break;
+    case U64:
+        switch(e->type)
+        {
+        case  I8: e->poly.u64 = e->poly.i8;  break; case  U8: e->poly.u64 = e->poly.u8;  break;
+        case I16: e->poly.u64 = e->poly.i16; break; case U16: e->poly.u64 = e->poly.u16; break;
+        case I32: e->poly.u64 = e->poly.i32; break; case U32: e->poly.u64 = e->poly.u32; break;
+        case I64: e->poly.u64 = e->poly.i64; break; case U64: e->poly.u64 = e->poly.u64; break;
+        case F32: e->poly.u64 = e->poly.f32; break; case F64: e->poly.u64 = e->poly.f64; break;
+        default: goto error;
+        }
+        break;
+    case F32:
+        switch(e->type)
+        {
+        case  I8: e->poly.f32 = e->poly.i8;  break; case  U8: e->poly.f32 = e->poly.u8;  break;
+        case I16: e->poly.f32 = e->poly.i16; break; case U16: e->poly.f32 = e->poly.u16; break;
+        case I32: e->poly.f32 = e->poly.i32; break; case U32: e->poly.f32 = e->poly.u32; break;
+        case I64: e->poly.f32 = e->poly.i64; break; case U64: e->poly.f32 = e->poly.u64; break;
+        case F32: e->poly.f32 = e->poly.f32; break; case F64: e->poly.f32 = e->poly.f64; break;
+        default: goto error;
+        }
+        break;
+    case F64:
+        switch(e->type)
+        {
+        case  I8: e->poly.f64 = e->poly.i8;  break; case  U8: e->poly.f64 = e->poly.u8;  break;
+        case I16: e->poly.f64 = e->poly.i16; break; case U16: e->poly.f64 = e->poly.u16; break;
+        case I32: e->poly.f64 = e->poly.i32; break; case U32: e->poly.f64 = e->poly.u32; break;
+        case I64: e->poly.f64 = e->poly.i64; break; case U64: e->poly.f64 = e->poly.u64; break;
+        case F32: e->poly.f64 = e->poly.f32; break; case F64: e->poly.f64 = e->poly.f64; break;
+        default: goto error;
+        }
+        break;
+    error:  
+    default:
+        quit("cast `%s: to `%s` not supported", Types[type], Types[e->type]);
+        break;
+    }
 }
 
 static bool
@@ -1490,6 +1607,7 @@ static Elem
 Call(Memb* m, vec_str* args)
 {
     // ENSURE ARGUMENT PARAMETER COUNT MATCHES.
+
     int got;
     {
         if(m->elem->type != FUN)
@@ -1625,10 +1743,12 @@ char*
 Open(const char* path)
 {
     FILE* file = fopen(path, "rb");
+    if(file == NULL)
+        quit("file `%s` not found", path);
     fseek(file, 0, SEEK_END);
     long bytes = ftell(file);
     rewind(file);
-    char* buffer = (char*) malloc(bytes * sizeof(char));
+    char* buffer = malloc(bytes);
     fread(buffer, bytes, 1, file);
     fclose(file);
     return buffer;
